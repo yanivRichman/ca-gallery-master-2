91e629b2e06525d96b6438754cb1f15882edfd70
yanivRichman <91840473+yanivRichman@users.noreply.github.com>
2021-10-18 21:56:50 +0300

    version 1.0

---
+++ js/proj-controller.js
@@ -0,0 +1,83 @@
+'use strict';
+
+function onInit() {
+    renderProjs();
+}
+
+function renderProjs() {
+    const projs = getProjs();
+    var strHtml = projs.map((proj) => {
+        return `
+    <div class="col-md-4 col-sm-6 portfolio-item">
+    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal" onclick="renderModal('${proj.id}')">
+    <div class="portfolio-hover">
+    <div class="portfolio-hover-content">
+    <i class="fa fa-plus fa-3x"></i>
+    </div>
+      </div>
+      <img class="img-fluid"
+       src="${proj.img}" 
+       alt="">
+    </a>
+    <div class="portfolio-caption">
+      <h4>${proj.name}</h4>
+      <p class="text-muted">${proj.title}</p>
+      </div>
+      </div>
+    `;
+    });
+    const elGallery = document.querySelector('.portfolio-container');
+    elGallery.innerHTML = strHtml.join('');
+}
+
+function renderModal(projId) {
+    const proj = getProjById(projId);
+    console.log(proj);
+    var strHtml = `
+    <div class="modal-dialog">
+            <div class="modal-content">
+    <div class="close-modal" data-dismiss="modal">
+      <div class="lr">
+        <div class="rl"></div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <div class="modal-body">
+            <!-- Project Details Go Here -->
+            <h2>${proj.name}</h2>
+            <p class="item-intro text-muted">
+              ${proj.title}
+            </p>
+            <img
+              class="img-fluid d-block mx-auto"
+              src="${proj.img}"
+              alt=""
+            />
+            <p>
+            ${proj.desc}
+            </p>
+            <ul class="list-inline">
+              <li>published At :${proj.publishedAt}</li>
+            </ul>
+            <button class="btn btn-primary" onclick="window.open('${proj.url}')">Check it out</button>
+            <button
+              class="btn btn-secondary"
+              data-dismiss="modal"
+              type="button"
+            >
+              <i class="fa fa-times"></i>
+              Close Project
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+`;
+    var elModal = document.querySelector('.portfolio-modal');
+    elModal.innerHTML = strHtml;
+}
---
+++ js/proj-service.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const gProjs = [
+    {
+        id: 'pacman',
+        name: 'Pacman',
+        title: 'Pacman is a maze action game developed and released by Namco for arcades in 1980',
+        desc: 'Your objective is that Pac-Man eat all of the pellets (white circles), while avoiding the ghosts that pursue him. If a ghost ever catches Pac-Man then Pac-Man is defeated',
+        img: '../img/portfolio/pacman.jpg',
+        url: 'https://yanivrichman.github.io/pacman/',
+        publishedAt: '30/09/21',
+        labels: ['Matrixes', 'keyboard events'],
+    },
+    {
+      id: 'minesweeper',
+      name: 'Minesweeper',
+      title: 'Minesweeper is a single-player puzzle video game.',
+      desc: 'The objective of the game is to clear a rectangular board containing hidden "mines" or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field',
+      img: '../img/portfolio/minesweeper.jpg',
+      url: 'https://yanivrichman.github.io/Minesweeper/',
+      publishedAt: '9/10/21',
+      labels: ['Matrixes', 'keyboard events'],
+  }
+];
+
+function getProjs() {
+    return gProjs;
+}
+
+function getProjById(projId) {
+  return gProjs.find((proj) => proj.id === projId);
+}
